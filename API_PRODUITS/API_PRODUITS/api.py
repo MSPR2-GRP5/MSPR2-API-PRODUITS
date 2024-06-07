476721597dc05a44c3f0f703b2b2e5318568d6ea
Anthony Tran <anthonytran275@yahoo.com>
2024-06-07 15:44:00 +0200

    Create produits table

--- API_PRODUITS/API_PRODUITS/api.py
+++ API_PRODUITS/API_PRODUITS/api.py
@@ -1,3 +1,31 @@
+from ninja import NinjaAPI, Schema
+
+api = NinjaAPI()
+
+class HelloSchema(Schema):
+    name: str = "world"
+
+@api.post("/hello")
+def hello(request, data: HelloSchema):
+    return f"Hello {data.name}"
+
+
 from ninja import NinjaAPI
 
 api = NinjaAPI()
+
+
+@api.get("/attempt")
+def attempt(request):
+    return "Attempted"
+
+@api.get("/math/{a}and{b}")
+def math(request, a: int, b: int):
+    return {"add": a + b, "multiply": a * b}
+
+class HelloSchema(Schema):
+    name: str = "world"
+
+@api.post("/hello")
+def hello(request, data: HelloSchema):
+    return f"Hello {data.name}"
--- API_PRODUITS/PRODUITS/models.py
+++ API_PRODUITS/PRODUITS/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class products(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    product_name = models.CharField(max_length=100)
+    import_location = models.CharField(max_length=60)
+    price = models.IntegerField()
+
+
+
+
 # Create your models here.
