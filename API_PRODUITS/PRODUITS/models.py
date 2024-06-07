1372b72e822bd98e126eccf462e18d826fbbe81b
Anthony Tran <anthonytran275@yahoo.com>
2024-06-07 16:08:16 +0200

    add forgotten description

--- API_PRODUITS/PRODUITS/models.py
+++ API_PRODUITS/PRODUITS/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class products(models.Model):
     id = models.BigAutoField(primary_key=True)
     product_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
     import_location = models.CharField(max_length=60)
     price = models.IntegerField()
     stocks = models.IntegerField()
